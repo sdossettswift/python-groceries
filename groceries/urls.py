c6f455849c4cb353d4f517ae0c43f27238a043ac
Sarah Swift <sdossettswift@gmail.com>
2017-05-24 16:59:43 -0500

    form to add, edit, must be logged in to add or edit

--- groceries/urls.py
+++ groceries/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
         url(r'^$', views.item_list, name='item_list'),
         url(r'^item/(?P<pk>\d+)/$', views.item_detail, name='item_detail'),
+        url(r'^item/new/$', views.item_new, name='item_new'),
+        url(r'^item/(?P<pk>\d+)/edit/$', views.item_edit, name='item_edit'),
 ]
--- groceries/views.py
+++ groceries/views.py
@@ -1,14 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,  get_object_or_404
 from django.utils import timezone
 from .models import Item
-from django.shortcuts import render, get_object_or_404
+from .forms import ItemForm
 
 
 def item_list(request):
-    items = Item.objects.filter(date_added__lte=timezone.now()).order_by('date_added')
+    items = Item.objects.filter(
+        date_added__lte=timezone.now()).order_by('date_added')
     return render(request, 'groceries/item_list.html', {'items': items})
 
 
 def item_detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     return render(request, 'groceries/item_detail.html', {'item': item})
+
+
+def item_new(request):
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.owner = request.user
+            item.date_added = timezone.now()
+            item.save()
+            return redirect('item_detail', pk=item.pk)
+    else:
+        form = ItemForm()
+    return render(request, 'groceries/item_edit.html', {'form': form})
+
+
+def item_edit(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.owner = request.user
+            item.date_added = timezone.now()
+            item.save()
+            return redirect('item_detail', pk=item.pk)
+    else:
+        form = ItemForm(instance=item)
+    return render(request, 'groceries/item_edit.html', {'form': form})
