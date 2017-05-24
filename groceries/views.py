6ca74b32f2927916ca716020bb2101184d18fceb
Sarah Swift <sdossettswift@gmail.com>
2017-05-24 14:28:44 -0500

    materialize installed,loaded

--- groceries/views.py
+++ groceries/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Item
 
 
 def item_list(request):
-    return render(request, 'groceries/item_list.html', {})
+    items = Item.objects.filter(date_added__lte=timezone.now()).order_by('date_added')
+    return render(request, 'groceries/item_list.html', {'items': items})
 
