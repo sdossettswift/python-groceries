41555dbe55a1f855046362629d6396c58986d56e
Sarah Swift <sdossettswift@gmail.com>
2017-05-24 15:55:00 -0500

    groceries templates, views, base template, detail view and list view

--- groceries/urls.py
+++ groceries/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.item_list, name='item_list'),
-        ]
+        url(r'^item/(?P<pk>\d+)/$', views.item_detail, name='item_detail'),
+]
--- groceries/views.py
+++ groceries/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Item
+from django.shortcuts import render, get_object_or_404
 
 
 def item_list(request):
     items = Item.objects.filter(date_added__lte=timezone.now()).order_by('date_added')
     return render(request, 'groceries/item_list.html', {'items': items})
 
+
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'groceries/item_detail.html', {'item': item})
