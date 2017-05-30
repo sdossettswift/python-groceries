7293f75fdee424db4b29826cad248211be314584
Sarah Swift <sdossettswift@gmail.com>
2017-05-30 12:56:57 -0500

    updated tests, views

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 # from django.template import loader #import template
 from django.urls import reverse
 from django.views import generic
+from django.utils import tiemzone
 
 from .models import Question, Choice
 
@@ -13,8 +14,10 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions(not including those set to be published in the future)."""
+        return Question.objects.filter(
+                pub_date__=timezone.now()
+                ).order_by('-pub_date')[:5]
 
 
 # def index(request):
